f4143de315dbec528d85e4f71b8ddb7e81cf5766
Souvik Sarkar <12345souvik12345@gmail.com>
2024-01-11 20:54:03 +0530

    Workspaces created

---
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -0,0 +1,11 @@
+import { OrganizationList } from "@clerk/nextjs";
+
+export default function CreateOrganizationalPage() {
+  return (
+    <OrganizationList
+      hidePersonal
+      afterSelectOrganizationUrl="/organization/:id"
+      afterCreateOrganizationUrl="/organization/:id"
+    />
+  );
+}
---
+++ config/site.js
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  name: "TaskHarbor",
+  description: "Collaborate,manage projects,and react new productivity peaks",
+};
